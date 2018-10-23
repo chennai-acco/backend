0d512ba9ca420e87fe56b477ad6eb8960801fe16
VimalRaj Selvam <email2vimalraj@gmail.com>
2018-10-24 06:31:58 +0800

    Added graphql-yoga server and initialized

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' })
+const createServer = require('./createServer')
+// const db = require('./db')
+
+const server = createServer()
+
+// TODO: use express middleware to handle cookies (jwt)
+// TODO: use express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => console.log(`Server running on http://localhost:${deets.port}`)
+)
